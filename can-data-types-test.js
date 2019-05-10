6417fe95726881befbed7689490f76d39aca55f3
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 19:12:04 +0100

    Landscaper: QUnit2 upgrade

--- can-data-types-test.js
+++ can-data-types-test.js
@@ -8,46 +8,46 @@ var isMemberSymbol = canSymbol.for("can.isMember");
 
 QUnit.module('can-data-types');
 
-QUnit.test('MaybeBoolean', function(){
-    QUnit.equal( canReflect.convert("true", dataTypes.MaybeBoolean), true, "converted");
+QUnit.test('MaybeBoolean', function(assert) {
+    assert.equal( canReflect.convert("true", dataTypes.MaybeBoolean), true, "converted");
 
-    QUnit.equal(canReflect.new(dataTypes.MaybeBoolean, "true") , true);
+    assert.equal(canReflect.new(dataTypes.MaybeBoolean, "true") , true);
 
-    QUnit.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")](true)    , true);
-    QUnit.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")]("true")  , false);
-    QUnit.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")](null) , true);
+    assert.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")](true)    , true);
+    assert.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")]("true")  , false);
+    assert.equal(dataTypes.MaybeBoolean[Symbol.for("can.isMember")](null) , true);
 });
 
-QUnit.test('MaybeDate', function(){
+QUnit.test('MaybeDate', function(assert) {
     var date = new Date(2018,0,1);
-    QUnit.equal( canReflect.convert(date.toString(), dataTypes.MaybeDate).getTime(), date.getTime(), "converted");
+    assert.equal( canReflect.convert(date.toString(), dataTypes.MaybeDate).getTime(), date.getTime(), "converted");
 
-    QUnit.deepEqual(canReflect.new(dataTypes.MaybeDate, "2018-1-31") , new Date( Date.parse("2018-1-31") ), "new" );
+    assert.deepEqual(canReflect.new(dataTypes.MaybeDate, "2018-1-31") , new Date( Date.parse("2018-1-31") ), "new" );
 
-    QUnit.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")](new Date()) , true);
-    QUnit.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")]({})  , false);
-    QUnit.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")](null) , true);
+    assert.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")](new Date()) , true);
+    assert.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")]({})  , false);
+    assert.equal(dataTypes.MaybeDate[Symbol.for("can.isMember")](null) , true);
 
 
 });
 
-QUnit.test('MaybeNumber', function(){
-    QUnit.equal( canReflect.convert("5", MaybeNumber), 5, "converted");
+QUnit.test('MaybeNumber', function(assert) {
+    assert.equal( canReflect.convert("5", MaybeNumber), 5, "converted");
 
-    QUnit.equal( canReflect.new(MaybeNumber, "1"), 1);
+    assert.equal( canReflect.new(MaybeNumber, "1"), 1);
 
-    QUnit.equal( MaybeNumber[isMemberSymbol](1)    , true);
-    QUnit.equal( MaybeNumber[isMemberSymbol]("1")  , false);
-    QUnit.equal( MaybeNumber[isMemberSymbol](null) , true);
+    assert.equal( MaybeNumber[isMemberSymbol](1)    , true);
+    assert.equal( MaybeNumber[isMemberSymbol]("1")  , false);
+    assert.equal( MaybeNumber[isMemberSymbol](null) , true);
 });
 
-QUnit.test('MaybeString', function(){
-    QUnit.equal( canReflect.convert(1, dataTypes.MaybeString), "1", "converted");
+QUnit.test('MaybeString', function(assert) {
+    assert.equal( canReflect.convert(1, dataTypes.MaybeString), "1", "converted");
 
-    QUnit.equal( canReflect.new(dataTypes.MaybeString, 1) , "1");
+    assert.equal( canReflect.new(dataTypes.MaybeString, 1) , "1");
 
-    QUnit.equal( dataTypes.MaybeString[Symbol.for("can.isMember")]("1")    , true);
-    QUnit.equal( dataTypes.MaybeString[Symbol.for("can.isMember")](1)  , false);
-    QUnit.equal( dataTypes.MaybeString[Symbol.for("can.isMember")](null) , true);
+    assert.equal( dataTypes.MaybeString[Symbol.for("can.isMember")]("1")    , true);
+    assert.equal( dataTypes.MaybeString[Symbol.for("can.isMember")](1)  , false);
+    assert.equal( dataTypes.MaybeString[Symbol.for("can.isMember")](null) , true);
 
 });
