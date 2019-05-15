e952015a710659307b2537e90c4f26f813d45146
Matthew Phillips <matthew@matthewphillips.info>
2019-05-15 13:56:49 -0400

    Handle dates with types.convert

--- types.js
+++ types.js
@@ -85,68 +85,13 @@ var createNoMaybe = makeTypeFactory(function createNoMaybe(Type, action, isMembe
 	});
 });
 
-/*
-
-function makeTypeWithAction(action) {
-	var typeCache = new Map();
-
-	return function createType(Type) {
-		if(typeCache.has(Type)) {
-			return typeCache.get(Type);
-		}
-
-		var isMember = function() { return false; };
-		if(primitives.has(Type)) {
-			isMember = primitives.get(Type).isMember;
-		}
-
-		var createNewOfType = function(val) {
-			if (val == null) {
-				return val;
-			}
-			if (val instanceof Type || isMember(val)) {
-				return val;
-			}
-			return action(Type, val);
-		}
-
-		var createTypeWithSchema = canReflect.assignSymbols(createNewOfType, {
-			"can.new": createNewOfType,
-			"can.getSchema": function(){
-				return {
-					type: "Or",
-					values: [Type, undefined, null]
-				};
-			},
-				"can.getName": function(){
-						return canReflect.getName(Type);
-				},
-			"can.isMember": function(value) {
-				return value == null || value instanceof Type || isMember(value);
-			}
-		});
-
-		typeCache.set(Type, createTypeWithSchema);
-
-		return createTypeWithSchema;
-	}
-}
-*/
-
 function check(Type, val) {
-	if(primitives.has(Type)) {
-		throw new Error(`Type value '${val}' is not of type ${canReflect.getName(Type)}.`);
-	}
-
-	return canReflect.new(Type, val);
+	throw new Error(`Type value '${val}' is not of type ${canReflect.getName(Type)}.`);
 }
 
 function convert(Type, val) {
-	if(primitives.has(Type)) {
-		return canReflect.convert(val, Type);
-	}
-
-	return canReflect.new(Type, val);
+	return canReflect.convert(val, Type);
+	//return canReflect.new(Type, val);
 }
 
 exports.check = createNoMaybe(check);
